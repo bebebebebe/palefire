157e48a2be5f57a8a87f430b98fd56773bafeccb
Be <bebirchall@gmail.com>
2013-05-23 17:27:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palefire::Application.config.secret_token = '287710c67ef651f4f3c510fb057781ebbcbd6da8418f02f52dead0fc9d91e9a52c3cd3db697a65924262a444962a6cfebebb1c8a12b216de96c440dd267e2993'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Palefire::Application.config.session_store :cookie_store, key: '_palefire_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Palefire::Application.config.session_store :active_record_store
