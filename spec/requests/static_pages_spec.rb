297c666f21c763b4c22bf57911510d4903a4fbc0
Be <bebirchall@gmail.com>
2013-05-23 22:51:48 -0400

    FInish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,26 +4,50 @@ describe "StaticPages" do
 
   describe "Home page" do
 
-    it "should have the content 'Pale Fire'" do
+    it "should have the h1 'Pale Fire'" do
       visit '/static_pages/home'
-      page.should have_content('Pale Fire')
+      page.should have_selector('h1', :text => 'Pale Fire')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Pale Fire | Home")
+    end
+
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Pale Fire | Help")
+    end
+
   end
 
   describe "About page" do
 
     it "should have the content 'About Pale Fire'" do
       visit '/static_pages/about'
-      page.should have_content('About Pale Fire')
+      page.should have_selector('h1', :text => 'About Pale Fire')
     end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Pale Fire | About")
+    end
+
   end
+
+
+
 
 end
