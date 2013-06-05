bb438c8a6bada4cd27084c4cc104458966624125
Be <bebirchall@gmail.com>
2013-06-05 16:31:57 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "welcome to Pale Fire"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 end
