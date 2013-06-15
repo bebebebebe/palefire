e7bbfcea2d66bbd2723d63271de40c85d38b5b02
Be <bebirchall@gmail.com>
2013-06-15 18:48:07 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -34,7 +35,7 @@ describe User do
     it { should be_invalid }
   end
 
-  describe "return value of autenticate method" do
+  describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) {User.find_by_email(@user.email) }
 
@@ -95,4 +96,10 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,7 +23,7 @@ describe "User pages" do
 
     before { visit try_path }
 
-    let(:submit) { "Create my account" }
+    let(:submit) { "create account" }
 
     describe "with invalid infomration" do
       it "should not create a user" do
@@ -33,15 +33,28 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "name:",         with: "Example User"
+        fill_in "email:",        with: "user@example.com"
+        fill_in "choose a password:",     with: "foobar"
+        fill_in "confirm password:", with: "foobar"
+      end
+
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'welcome') }
+        it { should have_link('Sign out') }
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+
+
     end
 
   end
