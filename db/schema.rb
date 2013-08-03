b7ff2d9e937d6725392e3aa1d502015bd14b1692
Be <bebirchall@gmail.com>
2013-08-03 13:19:21 -0400

    remove plots

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130731142031) do
+ActiveRecord::Schema.define(:version => 20130725152930) do
 
   create_table "cards", :force => true do |t|
     t.text     "writing"
     t.integer  "stack_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "plots", :force => true do |t|
-    t.integer  "project_id"
-    t.boolean  "pick"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
